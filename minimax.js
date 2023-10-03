7df7b1137018bfc076b570157eb2c0da6edef75e
Arthur Queiroz <arthurqueiroz25@gmail.com>
2023-10-02 23:24:18 -0300

    update minimax.js

--- minimax.js
+++ minimax.js
@@ -4,7 +4,7 @@ const startScreen = document.getElementById('startScreen');
 const board = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
 
-// Função para começar o jogo com a escolha do jogador
+
 function startGame(startingPlayer) {
   currentPlayer = startingPlayer;
   startScreen.style.display = 'none';
@@ -12,7 +12,7 @@ function startGame(startingPlayer) {
   renderBoard();
 
   if (startingPlayer === 'O') {
-    // Se o jogador escolher deixar a IA começar, a IA faz a primeira jogada
+    
     makeComputerMove();
   }
 }
@@ -68,25 +68,25 @@ function displayResult() {
 }
 
 function calculateResult(board) {
-// Verifique todas as possíveis combinações de vitória
+
 const winningCombinations = [
-[0, 1, 2], [3, 4, 5], [6, 7, 8], // Linhas
-[0, 3, 6], [1, 4, 7], [2, 5, 8], // Colunas
-[0, 4, 8], [2, 4, 6] // Diagonais
+[0, 1, 2], [3, 4, 5], [6, 7, 8], 
+[0, 3, 6], [1, 4, 7], [2, 5, 8], 
+[0, 4, 8], [2, 4, 6] 
 ];
 
 for (const combination of winningCombinations) {
 const [a, b, c] = combination;
 if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-  return board[a]; // Retorna o vencedor ('X' ou 'O')
+  return board[a]; 
 }
 }
 
 if (!board.includes('')) {
-return 'Empate'; // Empate se não houver células vazias
+return 'Empate'; 
 }
 
-return null; // O jogo ainda está em andamento
+return null; 
 }
 
 function isGameOver(board) {
@@ -102,7 +102,7 @@ for (const move of availableMoves) {
 const index = move.index;
 board[index] = player;
 const score = minimax(board, 0, false, player);
-board[index] = ''; // Desfaz a jogada
+board[index] = ''; 
 
 if ((player === 'X' && score > bestScore) || (player === 'O' && score < bestScore)) {
   bestScore = score;
